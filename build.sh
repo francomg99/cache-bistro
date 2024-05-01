03f441a3bbd3ea4c999ca719380da762a179c823
Francomg99 <francomartinezg99@gmail.com>
2024-05-01 15:50:35 +0200

    Primer version final

---
+++ build.sh
@@ -0,0 +1,9 @@
+.venv\Scripts\activate
+pip install --upgrade pip
+pip install -r requirements.txt
+reflex init
+reflex export --frontend-only
+rm -rf public
+unzip frontend.zip -d public
+rm -f frontend.zip
+desactivate
